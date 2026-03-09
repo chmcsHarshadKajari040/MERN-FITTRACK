d47bf728bbf6ca26a63c579b3ea736cacbd6b89e
chmcsHarshadKajari040 <chmcs2026.harshad.kajari@gmail.com>
2026-03-09 20:28:23 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000"
+  //baseURL: "http://localhost:3000"
+  baseURL: "https://mern-fittrack.onrender.com"
 });
 
 export default api;
